653b1a70443a7cf46efc3344f54ac6724de106df
Joey Ma <mustbuyinc@gmail.com>
2024-01-15 23:23:41 -0800

    (test) configure lint-staged

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,26 @@
+import micromatch from "micromatch";
+
+export default function (allStagedFiles) {
+  const shFiles = micromatch(allStagedFiles, ["**/src/**/*.sh"]);
+  if (shFiles.length) {
+    return `printf '%s\n' "Script files aren't allowed in src directory" >&2`;
+  }
+  const codeFiles = micromatch(allStagedFiles, [
+    "**/*.(c|m)js",
+    "**/*.js?(x)",
+    "**/*.ts?(x)",
+    "**/*.css",
+  ]).map((file) => `"${file}`);
+
+  const config = [];
+
+  if (codeFiles.length) {
+    config.push(`eslint --fix ${codeFiles.join(" ")}`);
+    config.push(`prettier --write ${codeFiles.join(" ")}`);
+  }
+  console.log("🚀 ~ lint-staged config:", config);
+
+  // const docFiles = micromatch(allStagedFiles, ["**/*.md"]);
+
+  return config;
+}
