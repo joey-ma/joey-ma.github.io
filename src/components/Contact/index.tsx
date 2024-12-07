7f98163821445f8a09581e3881f4df26db0dab25
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-06 16:21:52 -0800

    update cta

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,14 +9,13 @@ const Contact = () => {
           <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
               className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-              data-wow-delay=".15s
-              "
+              data-wow-delay=".15s"
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Need Help? Send a Message
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Our support team will get back to you ASAP via email.
+                I will get back to you ASAP via email.
               </p>
               <ContactForm />
             </div>
