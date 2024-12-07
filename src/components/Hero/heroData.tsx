4be58906f2d7e5f04fe26cc4a50613dbc2404ebe
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-06 16:14:18 -0800

    update hero section intro

--- src/components/Hero/heroData.tsx
+++ src/components/Hero/heroData.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/types/hero";
 
 const heroData: Hero = {
   title: <>Building Solutions,<br/>Crafting Experiences</>,
-  description: 'Hi, I’m Joey Ma, a Software Engineer specializing in building high-performance, user-centric web applications. I’m passionate about creating scalable solutions that make an impact, and I thrive at the intersection of creativity, technology, and problem-solving. Whether I am optimizing performance, leading migrations, or contributing to open-source projects, I believe in continuously improving and delivering value through every line of code.',
+  description: 'Hi, I’m Joey Ma, a Software Engineer specializing in high-performance, user-centric web applications. I’m passionate about impactful, scalable solutions and thrive at the intersection of creativity, technology, and problem-solving. Whether optimizing performance, leading migrations, or contributing to open-source projects, I focus on continuous improvement and delivering value through every line of code.',
   primaryCTA: {
     buttonText: 'Contact Me',
     link: '/contact',
