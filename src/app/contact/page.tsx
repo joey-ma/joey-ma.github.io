b548312dbebb61c5ae1aff26b9c257aad07e2ca3
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-06 16:11:55 -0800

    update content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,19 +4,17 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Contact Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Contact Page for Startup Nextjs Template",
-  // other metadata
+  title: "Let's Connect!",
+  description: "Have a question, want to collaborate, or hire me? Feel free to reach out!",
 };
 
 const ContactPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Let's Connect!"
+        description="Have a question, want to collaborate, or hire me? Feel free to reach out!"
       />
-
       <Contact />
     </>
   );
