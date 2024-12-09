89832fe02ea7caaa489c8179bf071ac4a6facf00
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-08 22:36:33 -0800

    separate out ContactForm

---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { validateForm } from "@/lib/utils";
+import { useState } from "react";
+
+export function ContactForm() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("🚀 ~ handleSubmit ~ formData:", formData)
+    const error = validateForm(formData);
+    console.log("🚀 ~ handleSubmit ~ error:", error)
+    if (error) {
+      alert(error);
+      return;
+    }
+    try {
+      const response = await fetch('/api/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        alert('Email sent successfully!');
+      } else {
+        alert('Failed to send email.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred while sending the email.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="-mx-4 flex flex-wrap">
+        <div className="w-full px-4 md:w-1/2">
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+            >
+              Your Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Enter your name"
+              className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+            />
+          </div>
+        </div>
+        <div className="w-full px-4 md:w-1/2">
+          <div className="mb-8">
+            <label
+              htmlFor="email"
+              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+            >
+              Your Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+              className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+            />
+          </div>
+        </div>
+        <div className="w-full px-4">
+          <div className="mb-8">
+            <label
+              htmlFor="message"
+              className="mb-3 block text-sm font-medium text-dark dark:text-white"
+            >
+              Your Message
+            </label>
+            <textarea
+              name="message"
+              rows={5}
+              value={formData.message}
+              onChange={handleChange}
+              placeholder="Enter your Message"
+              className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+            ></textarea>
+          </div>
+        </div>
+        <div className="w-full px-4">
+          <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,3 +1,4 @@
+import { ContactForm } from "./ContactForm";
 import NewsLatterBox from "./NewsLatterBox";
 
 const Contact = () => {
@@ -17,61 +18,7 @@ const Contact = () => {
               <p className="mb-12 text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
               </p>
-              <form>
-                <div className="-mx-4 flex flex-wrap">
-                  <div className="w-full px-4 md:w-1/2">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        Your Name
-                      </label>
-                      <input
-                        type="text"
-                        placeholder="Enter your name"
-                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full px-4 md:w-1/2">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        Your Email
-                      </label>
-                      <input
-                        type="email"
-                        placeholder="Enter your email"
-                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full px-4">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        Your Message
-                      </label>
-                      <textarea
-                        name="message"
-                        rows={5}
-                        placeholder="Enter your Message"
-                        className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div className="w-full px-4">
-                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                      Submit Ticket
-                    </button>
-                  </div>
-                </div>
-              </form>
+              <ContactForm />
             </div>
           </div>
           <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
