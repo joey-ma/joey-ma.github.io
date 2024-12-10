cb2c68784ba28e2bf243a3080c056970c5910533
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 15:57:35 -0800

    rm export const dynamic = 'force-dynamic'

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,6 +1,3 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { validateForm } from '@/lib/utils';
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server';
