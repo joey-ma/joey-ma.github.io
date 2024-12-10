222721324c4160de79513dc5389b09a9743e3c56
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 14:39:48 -0800

    accept paragraph as ReactNode for flexibility

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -8,7 +8,7 @@ const SectionTitle = ({
   mb = "100px",
 }: {
   title: string | ReactNode;
-  paragraph: string;
+  paragraph: string | ReactNode;
   width?: string;
   center?: boolean;
   mb?: string;
@@ -22,9 +22,9 @@ const SectionTitle = ({
         <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
+        {typeof paragraph === 'string' ? <p className="text-base !leading-relaxed text-body-color md:text-lg">
           {paragraph}
-        </p>
+        </p> : paragraph}
       </div>
     </>
   );
