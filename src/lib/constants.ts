adbdd76ff4cfed4d4ddd295761f191e47552f2fc
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 12:44:39 -0800

    add links to lib/constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const linkTo = {
+  gitHubPagesLimits: 'https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#limits-on-use-of-github-pages',
+  LinkedIn: 'https://www.linkedin.com/in/joeyma/',
+}
