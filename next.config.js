56809e41a2bfeb851c828160f64230b640c00a8e
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 11:34:32 -0800

    experimenting

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  exportPathMap: async function (defaultPathMap) {
+    // Exclude API routes from the static export
+    const filteredPathMap = Object.keys(defaultPathMap).reduce((acc, path) => {
+      if (!path.startsWith('/api')) {
+        acc[path] = defaultPathMap[path];
+      }
+      return acc;
+    }, {});
+
+    return filteredPathMap;
+  },
   images: {
     remotePatterns: [
       {
