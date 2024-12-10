7117601c772241ec51b9aaf8d50af02081cd1fcc
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 11:37:12 -0800

    use rewrites instead (The "exportPathMap" configuration cannot be used with the "app" directory. Please use generateStaticParams() instead.)

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  exportPathMap: async function (defaultPathMap) {
-    // Exclude API routes from the static export
-    const filteredPathMap = Object.keys(defaultPathMap).reduce((acc, path) => {
-      if (!path.startsWith('/api')) {
-        acc[path] = defaultPathMap[path];
-      }
-      return acc;
-    }, {});
-
-    return filteredPathMap;
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
   },
   images: {
     remotePatterns: [
