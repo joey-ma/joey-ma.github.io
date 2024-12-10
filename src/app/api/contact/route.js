3c0e0f7660dd18fb89e66d9b98fb005594894b14
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-10 11:27:30 -0800

    configure route to be dynamic

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { validateForm } from '@/lib/utils';
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server';
