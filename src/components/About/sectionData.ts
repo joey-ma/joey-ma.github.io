b50002dde5f1162006386baa6097428a30b20e1f
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-11 16:12:31 -0800

    add about section data

---
+++ src/components/About/sectionData.ts
@@ -0,0 +1,17 @@
+export const sectionOne = [
+  { title: 'Bug free code', description: ''}, 
+  { title: 'Collaborative', description: 'Participate in project ideation sessions, workshops, and roadmapping, collaborating in strategic planning that drives innovation from a development perspective'},
+  { title: 'Flexible', description: 'Participate in project ideation sessions, workshops, and roadmapping, collaborating in strategic planning that drives innovation from a development perspective'}
+]
+
+export const sectionTwo = [
+  { title: 'Iterative development', description: 'Work with the project team to validate goals and requirements, and provide input towards practical solutions, innovations, and estimation of effort and time'},
+  { title: 'Bug free code', description: 'Write, test, refactor, and optimize code to achieve high benchmarks for performance, and strive to make quality assurance a top priority'}, 
+  { title: 'Lifelong learner', description: 'Keep up-to-date with emerging technologies and patterns, and support the development of standards and processes that contribute to an environment of continual improvement'}
+]
+
+export const sectionThree = [
+  { title: 'Comprehensive support', description: 'Support the planning, development, testing, and delivery/deployment of solid, structured, and standards-compliant web and mobile interfaces'}, 
+  { title: 'On the same page', description: 'Understand our clients needs and requirements, and the technical opportunities and challenges that exist within their categories.'},
+  { title: 'On the same team', description: 'Participate in project ideation sessions, workshops, and roadmapping, collaborating in strategic planning that drives innovation from a development perspective'},
+]
