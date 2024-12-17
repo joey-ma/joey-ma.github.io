2a3811d984c1fa1d3094d6a46e2c963f5e9c6412
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-17 12:51:20 -0800

    init & add PostHogProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,20 @@
 
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always", // to create profiles for anonymous users as well
+  });
+}
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      {children}
+      <PostHogProvider client={posthog}>{children}</PostHogProvider>
     </ThemeProvider>
   );
 }
