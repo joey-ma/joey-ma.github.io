36d1e1fa7941ecad58b2ed0fbad4052add5175bc
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2024-12-17 16:44:17 -0800

    using next.js rewrites as a reverse proxy

--- next.config.js
+++ next.config.js
@@ -9,6 +9,23 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: process.env.NEXT_PUBLIC_POSTHOG_HOST + "/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: process.env.NEXT_PUBLIC_POSTHOG_HOST + "/decide",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
