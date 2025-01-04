60f3526e0556d8b43f53313094dfd5f8694f6ea3
Joey Ma <86270711+joey-ma@users.noreply.github.com>
2025-01-03 22:24:37 -0800

    feat(core): set up posthog with reverse proxy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,8 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: process.env.NEXT_PUBLIC_S_FN_API_HOST,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "always", // to create profiles for anonymous users as well
     persistence: "localStorage",
   });
